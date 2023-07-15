466730dc6c117ac76ed800c820ddbb7b3d90ecbc
Yusuf Ali Cezik <yusufalicezik35@gmail.com>
2023-07-15 23:11:43 +0300

    project has been created

---
+++ JoIn-Lite/JoIn_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoIn_LiteApp.swift
+//  JoIn-Lite
+//
+//  Created by Yusuf Ali Cezik on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct JoIn_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
