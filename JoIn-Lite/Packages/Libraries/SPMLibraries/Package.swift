9b77a53bc0aaf602f2f49fef92cd2eb4d71baae0
Yusuf Ali Cezik <yusufalicezik35@gmail.com>
2023-07-15 23:36:48 +0300

    packages added

---
+++ JoIn-Lite/Packages/Libraries/SPMLibraries/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SPMLibraries",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SPMLibraries",
+            targets: ["SPMLibraries"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/yusufalicezik/YACNetwork",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            .upToNextMajor(from: "7.0.0")
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SPMLibraries",
+            dependencies: ["YACNetwork", "Kingfisher"]
+        ),
+        .testTarget(
+            name: "SPMLibrariesTests",
+            dependencies: ["SPMLibraries"]),
+    ]
+)
