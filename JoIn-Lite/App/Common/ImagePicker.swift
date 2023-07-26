962cb4cd9c04d6827fadba566deb6bdffa430364
yusufalicezik <yusufalicezik35@gmail.com>
2023-07-26 12:52:22 +0300

    add imagepicker

---
+++ JoIn-Lite/App/Common/ImagePicker.swift
@@ -0,0 +1,42 @@
+//
+//  ImagePicker.swift
+//  JoIn-Lite
+//
+//  Created by Yusuf Ali Cezik on 26.07.2023.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: Image?
+    @Environment(\.presentationMode) var mode
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let uiImage = info[.originalImage] as? UIImage  else { return }
+            let image = Image(uiImage: uiImage)
+            parent.image = image
+            parent.mode.wrappedValue.dismiss()
+        }
+    }
+}
